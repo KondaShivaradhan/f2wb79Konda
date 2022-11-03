8ee5e8f7fd9ebf8a373a29d16f05d966e6b66259
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2022-11-02 21:06:50 -0500

    Simple fix

--- routes/institution.js
+++ routes/institution.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('institution', { title: 'Institution Search Results' });
+    res.render('institution', { title: 'Institutions Search Results' });
 });
 
 module.exports = router;
